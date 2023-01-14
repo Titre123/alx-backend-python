801296b3d7ea54a3f2f565c7a22f0e8ba11c71d9
Taiwo <taiwotriumphnat@gmail.com>
2023-01-14 04:38:02 +0100

    asyncio comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+    Import async_comprehension from the previous file and write
+    a measure_runtime coroutine that will execute async_comprehension
+    our times in parallel using asyncio.gather.
+    measure_runtime should measure the total runtime and return it.
+'''
+import time
+import asyncio
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    elapsed = time.perf_counter() - start
+    return elapsed
